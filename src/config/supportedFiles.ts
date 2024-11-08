bac04980ac535bbe183f399542331eab0cda34a3
Varunreddy <varunsannapureddy@gmail.com>
2024-11-08 23:44:05 +0530

    feat : add ratelimiting

--- src/config/supportedFiles.ts
+++
@@ -1,6 +0,0 @@
-export const supportedFiles = {
-  "application/pdf": "pdf",
-  "application/msword": "doc",
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-    "docx",
-};
